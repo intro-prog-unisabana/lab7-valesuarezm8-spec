09dda3a3703638696e12342ceeaeede94e23c706
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-04-15 19:42:12 -0500

    3

--- password_manager.py
+++ password_manager.py
@@ -25,6 +25,8 @@ def encrypt_passwords_in_file(filename: str) -> None:
         filas = list(reader)
 
     for i in range(1, len(filas)):
+        if not filas[i]:
+                continue
         filas[i][2] = caesar_encrypt(filas[i][2])
 
     with open(filename, 'w', newline='') as file:
@@ -44,14 +46,13 @@ def change_password(filename: str, website: str, password: str) -> bool:
                 filas[i][2] = caesar_encrypt(password)
                 encontrado = True
                 break
-            if not encontrado:
-                    return False
+    if not encontrado:
+        return False
             
     with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(filas)
     return True
-
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
     """TODO: Parte 4."""
